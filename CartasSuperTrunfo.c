bfd8edcdc2a5a0cd1bc18a0bd4e6374c9ede932f
DavidSilvaDionisio <daviddionisio10@gmail.com>
2025-01-18 19:00:51 +0000

    Acrescentando novas funções.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,27 +6,51 @@
 
 int main() {
 
-// Declaração de variáveis.
-int populacao;
-int NumPontosTuristicos;
-float area; 
-float pib;
+// Declaração de variáveis. 
+
+         char Estado;
+         char CodCarta [3];
+         char Cidade  [20];
+         int Populacao;
+         float Area; 
+         float DensidadePopulacional;
+         float Pib;
+         float PibPerCapito;
+         int PontosTuristicos;
+
+
+
 
 // printf mostra menssagem na tela pedindo que o usuario entre com o dado. scanf pega esse dado e guarda na variável.
- printf(" Digite o número da população: "); 
-   scanf("%d%*c", &populacao);
 
-printf(" Digite o número de pontos turísticos: "); 
-   scanf("%d%*c", &NumPontosTuristicos);
+ printf(" Digite a letra do  Estado: "); 
+   scanf("%c%*c", &Estado);
+
+   printf(" Digite o código da carta: "); 
+   scanf("%s%*c", &CodCarta);
+
+printf(" Digite o nome da Cidade: "); 
+  scanf("%s%*c",Cidade);
+
+  printf(" Digite a População da Cidade: "); 
+   scanf("%d%*c", &Populacao);
 
 printf(" Digite a área da Cidade: "); 
-   scanf("%f%*c", &area);
+   scanf("%f%*c", &Area);
 
-printf(" Digite o PIB:"); 
-   scanf("%f%*c", &pib);
+   printf(" Digite o PIB: "); 
+   scanf("%f%*c", &Pib);
+
+  printf(" Digite o o número de pontos turísticos da Cidade: "); 
+   scanf("%f%*c", &PontosTuristicos);
+
+
+ DensidadePopulacional = Populacao /  Area;
+
+ PibPerCapito = Pib / Populacao;
 
 // printf mostra a saida dos dados e sua formatação.
-printf(" População: %d\n Pontos turísticos: %d\n Área: %6.3fkm²\n PIB: %3.1f%\n ",populacao, NumPontosTuristicos, area, pib); 
+printf(" Estado: %c\n Cidade: %c\n População: %d\n Area: %f\n  Densidade Populacional: %f\n  PIB: %f\n PIB Per Capito: %f\n Pontos Turísticos: %d\n ",  Estado,  Cidade, Populacao, Area, DensidadePopulacional, Pib, PibPerCapito,  PontosTuristicos  ); 
 
 //Printf mosta a mensagem dados cadastrado com sucesso.
 printf("Dados cadastrados com sucesso.\n"); 
