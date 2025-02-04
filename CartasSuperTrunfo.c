7a25ba8208680e08e65c4435de95d77b8004b013
DavidSilvaDionisio <daviddionisio10@gmail.com>
2025-02-04 17:11:37 +0000

    Programa 95% comcluido.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h> // A diretiva #include faz com que o texto contido na biblioteca especificada ser inserido no programa.
 
 
@@ -14,37 +12,39 @@ int main() {   // Função principal.
    char Estado2, CodCarta2 [3], Cidade2 [20], NomeJogador2 [20];
    int Populacao2, PontoTuristico2, Ponto2;    
    float  Area2, DensidadePopulacional2, PIB_Percapito2, PIB2; 
+
+ // variavel de controle.
+
    int Opcao;
-   
+   int Num1;
+ // Inicialiacao de variaveis.
+
    Ponto1 = 0;
    Ponto2 = 0;
-   
-
-   // 
-
-printf(">>>>>>>>> Super trunfo. <<<<<<<<<\n");
-printf("**********************************************************\n");
-
-//Entrada e amarzenamento de dados.
-
-printf("Digite 1, para iniciar o jogo\n");
-printf("Digite 2, para sair do menu.\n");
 
 
-switch (Opcao)
+   printf(">>>>>>>>> Super trunfo. <<<<<<<<<\n");
+   printf("**********************************************************\n");
+
+ // Entrada e amarzenamento de dados.
+
+   printf("Digite 1, para comparação de números de pessoas que residem na Cidade.\n");
+   printf("Digite 2, para comparação de Pontos turísticos.\n");
+   printf("Digite 3, para a comparação da área.\n");
+   printf("Digite 4, para comparação do PIB.\n");
+   printf("Digite 5, para comparação PIB Percapito.\n");
+   printf("Digite 6, para comparação da Densidade Populacional.\n");
+   printf("Digite 7, para Comparação de todos o itens.\n");
+   printf("Digite 8, sair.\n");
+   scanf("%d%*c", &Num1);
+
+
+// Estrutura Condicional
+
+   switch (Num1)
 {
+
 case 1:
-   printf("Digite 1, para comparação Populacional.");
-   printf("Digite 2, para comparação de Pontos turísticos..");
-   printf("Digite 3, para a comparação do tamanho da área.");
-   printf("Digite 4, para comparação da Densidade Populacional.");
-   printf("Digite 5, para comparação do PIB.");
-   printf("Digite 6, para comparação PIB Percapito.");
-   printf("Digite 7, para comparação de todos os itens.");
-   printf("Digite 8, para sair do menu.");
-switch (Opcao)
-{
-   case 1:
    
 printf(" Jogador 1, digite o seu nome: "); 
 scanf("%s%*c", &NomeJogador1);
@@ -52,18 +52,16 @@ printf("\n" );
 printf("Entre com os dados da primeira carta.\n");;  
 printf("\n" );  
 
-
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado1);
-
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta1);
-
 
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade1);
 
-printf("Digite o número de pessoas que residem na Cidade.\n");  
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta1);
+
+printf("Digite o número de pessoas que moram na Cidade.\n");  
 scanf ("%d%*c", &Populacao1);
 
 printf(" Jogador 2, digite o seu nome: "); 
@@ -76,37 +74,42 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado2);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta2);
-
 
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade2);
 
-printf("Digite o número de pessoas que residem na Cidade.\n");  
+
+printf("Digite codigo da Cidade.\n");  
+scanf ("%s%*c", &CodCarta2);
+
+printf("Digite o número de pessoas que moram na Cidade.\n");  
 scanf ("%d%*c", &Populacao2);
 
-if (Populacao1 > Populacao2)
-{
+if (Populacao1 > Populacao2){
+
  printf("\n");  
  printf("Parabéns %s, você Venceu!!\n", NomeJogador1);  
  printf("\n");  
  
-}else if(Populacao2 > Populacao1){
+}else if(Populacao1 < Populacao2){
 
-printf("\n");  
- printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
  printf("\n");  
+ printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
+ printf("\n");
+  
 }else{
 
-printf("\n");  
+ printf("\n");  
  printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
  printf("\n");  
 }
-//-----------------------------------------------------------------------------------------------------------------------------------------------
-   break;
 
-   case 2:
+break;
+
+//-----------------------------------------------------------------------------------------
+   
+
+case 2:
    
 
 printf(" Jogador 1, digite o seu nome: "); 
@@ -119,12 +122,11 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado1);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta1);
-
-
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade1);
+
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta1);
 
 printf("Digite o número de pontos turísticos da cidade.\n");  
 scanf ("%d%*c", &PontoTuristico1);
@@ -139,38 +141,43 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado2);
 
+printf("Digite o nome da Cidade.\n");  
+scanf ("%s%*c", &Cidade2);
+
+
 printf("Digite o código da cidade.\n");  
 scanf ("%s%*c", &CodCarta2);
 
 
-printf("Digite o nome da Cidade.\n");  
-scanf ("%s%*c", &Cidade2);
-
 printf("Digite o número de pontos turísticos da cidade.\n");  
-scanf ("%d%*c", &PontoTuristico1);
+scanf ("%d%*c", &PontoTuristico2);
 
 
-if (PontoTuristico1 > PontoTuristico2)
-{
+if (PontoTuristico1 > PontoTuristico2){
+
  printf("\n");  
  printf("Parabéns %s, você Venceu!!\n", NomeJogador1);  
  printf("\n");  
  
 }else if(PontoTuristico1 < PontoTuristico2){
 
-printf("\n");  
- printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
  printf("\n");  
+ printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
+ printf("\n"); 
+
 }else{
 
-   printf("\n");  
+ printf("\n");  
  printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
  printf("\n");  
+
 }
-//----------------------------------------------------------------------------------------------------------------------
+
 break;
 
-   case 3:
+//-----------------------------------------------------------------------------------------
+
+case 3:
    
 printf(" Jogador 1, digite o seu nome: "); 
 scanf("%s%*c", &NomeJogador1);
@@ -182,15 +189,14 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado1);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta1);
-
-
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade1);
 
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta1);
+
 printf("Digite a área da cidade.\n");  
-scanf ("%d%*c", &Area1);
+scanf ("%f%*c", &Area1);
 
 printf(" Jogador 2, digite o seu nome: "); 
 scanf("%s%*c", &NomeJogador2);
@@ -202,16 +208,14 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado2);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta2);
-
-
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade2);
 
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta2);
+
 printf("Digite a área da cidade.\n");  
 scanf ("%d%*c", &Area2);
-
 
 if (Area1 > Area2)
 {
@@ -222,21 +226,23 @@ if (Area1 > Area2)
 }else if(Area1 < Area2){
 
 printf("\n");  
- printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
- printf("\n");  
+printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
+printf("\n");  
 }else{
 
-   printf("\n");  
- printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
- printf("\n");  
+printf("\n");  
+printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
+printf("\n");  
+
 }
 
  
 //----------------------------------------------------------------------------------------------------------------------------------------
    break;
 
-   case 4:
-   printf(" Jogador 1, digite o seu nome: "); 
+case 4:
+   
+printf(" Jogador 1, digite o seu nome: "); 
 scanf("%s%*c", &NomeJogador1);
 printf("\n" );
 printf("Entre com os dados da primeira carta.\n");;  
@@ -246,15 +252,14 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado1);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta1);
-
-
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade1);
 
-printf("Digite a Densidade populacional.\n");  
-scanf ("%d%*c", DensidadePopulacional1);
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta1);
+
+printf("Digite o PIB  da Cidade.\n");  
+scanf ("%d%*c", &PIB1);
 
 printf(" Jogador 2, digite o seu nome: "); 
 scanf("%s%*c", &NomeJogador2);
@@ -266,78 +271,14 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado2);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta2);
-
-
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade2);
-
-printf("Digite a Densidade populacional.\n");  
-scanf ("%d%*c", DensidadePopulacional2);
-
-if (DensidadePopulacional1 < DensidadePopulacional2)
-{
- printf("\n");  
- printf("Parabéns %s, você Venceu!!\n", NomeJogador1);  
- printf("\n");  
- 
-}else if(DensidadePopulacional1 > DensidadePopulacional1){
-
-printf("\n");  
- printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
- printf("\n");  
-}else{
-
-   printf("\n");  
- printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
- printf("\n");  
-}
-
-
-//-------------------------------------------------------------------------------------------------------------------------------
-   break;
-
-   case 5:
-   printf(" Jogador 1, digite o seu nome: "); 
-scanf("%s%*c", &NomeJogador1);
-printf("\n" );
-printf("Entre com os dados da primeira carta.\n");;  
-printf("\n" );  
-
-
-printf("Digite o nome do Estado.\n");  
-scanf ("%c%*c", &Estado1);
-
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta1);
-
-
-printf("Digite o nome da Cidade.\n");  
-scanf ("%s%*c", &Cidade1);
-
-printf("Digite o PIB.\n");  
-scanf ("%d%*c", &Populacao1);
-
-printf(" Jogador 2, digite o seu nome: "); 
-scanf("%s%*c", &NomeJogador2);
-printf("\n" );
-printf("Entre com os dados da segunda carta.\n");;  
-printf("\n" );  
-
-
-printf("Digite o nome do Estado.\n");  
-scanf ("%c%*c", &Estado2);
 
 printf("Digite o código da cidade.\n");  
 scanf ("%s%*c", &CodCarta2);
 
-
-printf("Digite o nome da Cidade.\n");  
-scanf ("%s%*c", &Cidade2);
-
-printf("Digite o PIB\n");  
-scanf ("%d%*c", &Populacao2);
+printf("Digite o PIB da Cidade.\n");  
+scanf ("%d%*c", &PIB2);
 
 if (PIB1 > PIB2)
 {
@@ -347,12 +288,77 @@ if (PIB1 > PIB2)
  
 }else if(PIB1 < PIB2){
 
-printf("\n");  
+ printf("\n");  
  printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
  printf("\n");  
 }else{
 
-   printf("\n");  
+ printf("\n");  
+ printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
+ printf("\n");  
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+   
+break;
+
+   
+case 5:
+   
+printf(" Jogador 1, digite o seu nome: "); 
+scanf("%s%*c", &NomeJogador1);
+printf("\n" );
+printf("Entre com os dados da primeira carta.\n");;  
+printf("\n" );  
+
+
+printf("Digite o nome do Estado.\n");  
+scanf ("%c%*c", &Estado1);
+
+
+printf("Digite o nome da Cidade.\n");  
+scanf ("%s%*c", &Cidade1);
+
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta1);
+
+printf("Digite o PIB Percapito.\n");  
+scanf ("%f%*c", &PIB_Percapito1);
+
+printf(" Jogador 2, digite o seu nome: "); 
+scanf("%s%*c", &NomeJogador2);
+printf("\n" );
+printf("Entre com os dados da segunda carta.\n");;  
+printf("\n" );  
+
+
+printf("Digite o nome do Estado.\n");  
+scanf ("%c%*c", &Estado2);
+
+printf("Digite o nome da Cidade.\n");  
+scanf ("%s%*c", &Cidade2);
+
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta2);
+
+printf("Digite o PIB Percapito\n");  
+scanf ("%f%*c", & PIB_Percapito2);
+
+if (  PIB_Percapito1 >  PIB_Percapito2)
+{
+ printf("\n");  
+ printf("Parabéns %s, você Venceu!!\n", NomeJogador1);  
+ printf("\n");  
+ 
+}else if( PIB_Percapito1 <  PIB_Percapito2){
+
+printf("\n");  
+printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
+printf("\n");  
+}else{
+
+ printf("\n");  
  printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
  printf("\n");  
 }
@@ -371,15 +377,16 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado1);
 
+printf("Digite o nome da Cidade.\n");  
+scanf ("%s%*c", &Cidade1);
+
+
 printf("Digite o código da cidade.\n");  
 scanf ("%s%*c", &CodCarta1);
 
 
-printf("Digite o nome da Cidade.\n");  
-scanf ("%s%*c", &Cidade1);
-
-printf("Digite o PIB Percapito.\n");  
-scanf ("%d%*c", &PIB_Percapito1);
+printf("Digite a Densidade Populacional.\n");  
+scanf ("%f%*c", &DensidadePopulacional1);
 
 printf(" Jogador 2, digite o seu nome: "); 
 scanf("%s%*c", &NomeJogador2);
@@ -391,30 +398,30 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado2);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta2);
-
-
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade2);
 
-printf("Digite o PIB Percapito.\n");  
-scanf ("%d%*c", &PIB_Percapito2);
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta2);
 
-if (PIB_Percapito1 > PIB_Percapito2)
+printf("Digite o a Densidade populacional.\n");  
+scanf ("%f%*c", &DensidadePopulacional2);  
+
+
+if ( DensidadePopulacional1 <  DensidadePopulacional2)
 {
  printf("\n");  
  printf("Parabéns %s, você Venceu!!\n", NomeJogador1);  
  printf("\n");  
  
-}else if(PIB_Percapito1 < PIB_Percapito2){
+}else if( DensidadePopulacional1 >  DensidadePopulacional2){
 
 printf("\n");  
  printf("Parabéns %s, você Venceu!!\n", NomeJogador2);  
  printf("\n");  
 }else{
 
-   printf("\n");  
+ printf("\n");  
  printf("Parabéns %s e %s, vocês Empataram!!\n", NomeJogador1, NomeJogador2);  
  printf("\n");  
 }
@@ -423,7 +430,7 @@ printf("\n");
    break;
 
    case 7:
-   printf(" Jogador 1, digite o seu nome: "); 
+printf(" Jogador 1, digite o seu nome: "); 
 scanf("%s%*c", &NomeJogador1);
 printf("\n" );
 printf("Entre com os dados da primeira carta.\n");;  
@@ -433,12 +440,12 @@ printf("\n" );
 printf("Digite o nome do Estado.\n");  
 scanf ("%c%*c", &Estado1);
 
-printf("Digite o código da cidade.\n");  
-scanf ("%s%*c", &CodCarta1);
-
-
 printf("Digite o nome da Cidade.\n");  
 scanf ("%s%*c", &Cidade1);
+
+
+printf("Digite o código da cidade.\n");  
+scanf ("%s%*c", &CodCarta1);
 
 printf("Digite o número de pessoas que residem na Cidade.\n");  
 scanf ("%d%*c", &Populacao1);
@@ -587,47 +594,40 @@ else
 {
    printf("\n"); 
    printf("Parabéns %s, você Ganhou o jogo!\n", NomeJogador2);  
-   printf("\n");  
+   printf("\n");
 
+ break;
+default:
+printf("Opção invalidada.");
+   break;
 
+case 8:
+ printf("Saindo...");
+ break;
 
-
+//default:
+ printf("Opção inválida!!");
+   break;
+   
 }
 
- 
 printf(">>>>>>>>> Super trunfo. <<<<<<<<<\n");
 printf("**********************************************************\n");
 printf("Fim do jogo.\n");
 
-
-    
 }
 
-   break;
 
-   case 8:
- printf("Saindo...");
-   break;
-
-default:
-printf("Opção invalidada.");
-   break;
+ 
+ 
 }
-{
+   
 
-{
-case 2:
-   printf("Saindo...");
-   break;
+   
 
-default:
-printf("Opção invalidada.");
-   break;
-}
-
-}
-
-//-------------------------------------------------------------------------------------------------------------------------------
+   
 
 
+
+//---------------------------
 
